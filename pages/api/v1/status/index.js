3a1995ae10c34cf84ae6a4a0f399a7cf07bfe5b6
Jainaldo Silva <jainaldosilva@gmail.com>
2025-07-31 00:20:49 +0000

    fix `prettier` style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,7 +7,7 @@ async function status(request, response) {
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query(
-    "SHOW max_connections;"
+    "SHOW max_connections;",
   );
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
@@ -15,7 +15,7 @@ async function status(request, response) {
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
+    values: [databaseName],
   });
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
@@ -27,9 +27,9 @@ async function status(request, response) {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
-      }
-    }
-  })
+      },
+    },
+  });
 }
 
 export default status;
