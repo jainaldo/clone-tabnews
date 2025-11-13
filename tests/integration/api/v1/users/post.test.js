deba3cfe5543f338cf4ea2bf20c93ce94b8f69f1
Jainaldo Silva <jainaldosilva@gmail.com>
2025-11-13 14:33:17 +0000

    feat: hash `password` in `user.create`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -27,15 +29,29 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "jainaldo1",
         email: "Contato@gmail.com",
-        password: "123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
+      expect(response.status).toBe(201);
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
-      expect(response.status).toBe(201);
+
+      const userInDatabase = await user.findOneByUsername("jainaldo1");
+      const correctPasswordMatch = await password.compare(
+        "123",
+        userInDatabase.password,
+      );
+      const incorrectPasswordMatch = await password.compare(
+        "SenhaErrada",
+        userInDatabase.password,
+      );
+
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
     });
+
     test("With duplicated 'email'", async () => {
       const response1 = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
